f3e09c27a65e827e9b97e46d035b18a2b2c0a68b
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-30 00:29:42 +0200

    playing chess

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -3,5 +3,11 @@ class Piece
   attr_accessor :pos
 
   def initialize(color, board, pos) 
+
     @color, @board, @pos = color, board, pos 
+    board.add_piece(self, pos) 
+  end 
+
+  def to_s 
+    " #{symbol} " 
   end 
