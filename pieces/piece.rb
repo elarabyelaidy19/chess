372f1a5737df3149122c0394d4029b5cea4f44ed
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-30 00:36:30 +0200

    playing chess

--- pieces/piece.rb
+++ pieces/piece.rb
@@ -11,3 +11,22 @@ class Piece
   def to_s 
     " #{symbol} " 
   end 
+
+  def empty? 
+    false 
+  end 
+
+  def symbol
+    raise NotImplementedError 
+  end 
+
+  def valid_moves 
+    moves.reject { |end_pos| move_into_check?(end_pos) }
+  end 
+
+  def move_into_check?(end_pos) 
+    test_board = board.dup 
+    test_board = board.move_piece!(pos, end_pos) 
+    test_board.in_check?(color) 
+  end 
+end 
