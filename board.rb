0203690609e7794639807f776fc85fb7aa4ff1c7
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-30 00:17:24 +0200

    playing chess

--- board.rb
+++ board.rb
@@ -9,9 +9,20 @@ class Board
   end 
   
   def [](pos) 
+    raise "invalid position" unless valid_pos?(pos)
 
     row, col = pos 
     @rows[row][col]
   end 
 
-  
+  def [](pos, piece) 
+    raise "invalid position" unless valid_pos?(pos)
+    row, col = pos 
+    @row[row][col] = piece 
+  end 
+
+  def add_piece(pos, piece) 
+    raise 'position not empty' unless empty?(pos) 
+    self[pos] = piece 
+  end 
+
