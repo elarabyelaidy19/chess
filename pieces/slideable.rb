0e7680c8e1e514011c6ffc805fe6b46c012d0be8
elaraby elaidy <elarabyelaidy@gmail.com>
2021-06-30 02:00:07 +0200

    playing chess

--- pieces/slideable.rb
+++ pieces/slideable.rb
@@ -31,6 +31,28 @@ module Slideable
     moves 
   end 
 
+  private
+
   def move_dirs 
     raise NotImplementedError 
   end 
+
+  def grow_unblocked_moves_in_dir(dx, dy) 
+    cur_x, cur_y = pos 
+    moves = [] 
+    loop do  
+      cur_x, cur_y = cur_x + dx, cur_y +dy 
+      pos = [cur_x, cur_y] 
+
+     break unless board.valid_pos?(pos) 
+
+     if board.empty?(pos) 
+      moves << pos 
+     else  
+      moves << pos if board[pos].color != color 
+      break 
+     end 
+    end 
+    moves 
+  end 
+end 
